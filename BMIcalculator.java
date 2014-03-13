d7081f35d8f0bf5e1e2ab729606813edc54fcc69
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-13 12:37:25 +0800

    建立文字方塊
    建立可供使用者輸入的文字方塊以及顯示結果的文字方塊

--- BMIcalculator.java
+++ BMIcalculator.java
@@ -4,5 +4,10 @@ import javax.swing.*;
 import javax.swing.border.TitleBorder;
 
 public class BMIcalculator extends JFrame {
-
+  private JtextField jtfName = new JtextField();
+	private JtextField jtfAge = new JtextField();
+	private JtextField jtfWeight = new JtextField();
+	private JtextField jtfHeight = new JtextField();
+	private JtextField jtfBMI = new JtextField();
+	private JtextField jtfStatus = new JtextField();
 }
