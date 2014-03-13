5770f51a220ae38bedde29f833d9c41acfe4e943
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-13 12:47:27 +0800

    建立介面內容
    此部分建立Label 以及第1個Panel(用來把握住名字、年齡、體重、身高、身體質量數、以及身體質量狀態)

--- BMIcalculator.java
+++ BMIcalculator.java
@@ -12,4 +12,20 @@ public class BMIcalculator extends JFrame {
 	private JtextField jtfStatus = new JtextField();
 	
 	private JButton jbtComputeBMI = new JButton("Compute result");
+	
+	public BMIcalculator() {
+		JPanel p1 = new JPanel(new GridLayout(6,2));
+		p1.add(new jLabel("Name"));
+		p1.add(jtfName);
+		p1.add(new jLabel("Age"));
+		p1.add(jtfAge);
+		p1.add(new jLabel("Weight"));
+		p1.add(jtfWeight);
+		p1.add(new jLabel("Height"));
+		p1.add(jtfHeight);
+		p1.add(new jLabel("BMI"));
+		p1.add(jtfBMI);
+		p1.add(new jLabel("Status"));
+		p1.add(jtfStatus);
+	}
 }
